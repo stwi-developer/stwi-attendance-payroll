002b3d3c37b804eb41825b685e53b7347f72cda4
stwi-developer <stwi.developer@gmail.com>
2026-09-16 14:38:00 +0530

    Port added

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   await prisma.enableShutdownHooks(app);
 
   const port = Number(process.env.PORT ?? 4000);
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 }
 
 void bootstrap();
